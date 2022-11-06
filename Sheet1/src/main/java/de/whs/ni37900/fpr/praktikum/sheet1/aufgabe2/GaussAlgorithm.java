a58043bbd5127eedd1aac5d01a148e3fafdc2ee6
Nik Benson <nbenson@nettrek.de>
2022-11-06 21:11:56 +0100

    feat(sheet1): Optimierung des Gauss Algorithmus

--- Sheet1/src/main/java/de/whs/ni37900/fpr/praktikum/sheet1/aufgabe2/GaussAlgorithm.java
+++ Sheet1/src/main/java/de/whs/ni37900/fpr/praktikum/sheet1/aufgabe2/GaussAlgorithm.java
@@ -35,22 +35,47 @@ public class GaussAlgorithm implements LGSSolver {
     private double[] solveNormalized(LGS lgs) throws NotSolvableException {
         if (lgs.getDegree() == 1) return solveFirstDegree(lgs);
 
-        double[] resultWithoutLastParameter = solve(reduceDegree(lgs));
+        int keepIndex = indexOfMaxAbsoluteFirstFactor(lgs);
 
-        Equation first = lgs.getEquations()[0];
-        double[] factors = first.getFactors();
+        double[] resultWithoutLastParameter = solve(reduceDegree(lgs, keepIndex));
+
+        Equation keep = lgs.getEquations()[keepIndex];
+        double[] factors = keep.getFactors();
 
         double sum = 0;
         for (int i = 0; i < resultWithoutLastParameter.length; i++) {
             sum += resultWithoutLastParameter[i] * factors[i];
         }
 
-        double missingResultParameter = (first.getResult() - sum) / factors[factors.length - 1];
+        double missingResultParameter = (keep.getResult() - sum) / factors[factors.length - 1];
 
         double[] result = Arrays.copyOf(resultWithoutLastParameter, resultWithoutLastParameter.length + 1);
         result[result.length - 1] = missingResultParameter;
 
         return result;
+    }
+
+    /**
+     * Ermittelt den Index der Gleichung im übergebenen LGS, die den größten, absoluten Wert des ersten Faktors hat.
+     * @param lgs das LGS, auf das sich der Index bezieht
+     * @return den index der Gleichung im übergebenen LGS, die den größten, absoluten Wert des ersten Faktors hat
+     */
+    private int indexOfMaxAbsoluteFirstFactor(LGS lgs) {
+        Equation[] equations = lgs.getEquations();
+
+        int maxIndex = 0;
+        double max = equations[0].getFactors()[0];
+
+        for (int currentIndex = 1; currentIndex < equations.length; currentIndex++) {
+            double current = Math.abs(equations[currentIndex].getFactors()[0]);
+
+            if(current > max) {
+                maxIndex = currentIndex;
+                max = current;
+            }
+        }
+
+        return maxIndex;
     }
 
     /**
@@ -94,16 +119,19 @@ public class GaussAlgorithm implements LGSSolver {
      * Generiert aus dem übergebenen LGS ein Teil-LGS des Grades {@code lgs.getDegree()} - 1
      *
      * @param lgs das zu reduzierende LGS
+     * @param keepIndex der Index der Gleichung, die nicht in das reduzierte LGS inkludiert werden soll
      * @return ein Teil-LGS von {@code lgs} des Grades {@code lgs.getDegree()} - 1
      */
-    private LGS reduceDegree(LGS lgs) {
+    private LGS reduceDegree(LGS lgs, int keepIndex) {
         Equation[] equations = lgs.getEquations();
-        Equation first = equations[0];
+        Equation keep = equations[keepIndex];
 
         LGS reduced = new LGS();
 
-        for (int i = 1; i < equations.length; i++) {
-            Equation reducedEquation = eliminateNthFactor(first, equations[i], lgs.getDegree() - 1);
+        for (int i = 0; i < equations.length; i++) {
+            if (i == keepIndex) continue;
+
+            Equation reducedEquation = eliminateNthFactor(keep, equations[i], lgs.getDegree() - 1);
             double[] factors = reducedEquation.getFactors();
             reduced.add(new Equation(Arrays.copyOf(factors, factors.length - 1), reducedEquation.getResult()));
         }
